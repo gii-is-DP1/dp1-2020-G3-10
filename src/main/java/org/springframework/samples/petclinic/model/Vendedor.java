c79a771cb51fb2582095932b198637b20f5ec8e6
guipavvar <guipavvar@alum.us.es>
2021-02-04 13:02:42 +0100

    Validacion de valoracion de vendedor

--- src/main/java/org/springframework/samples/petclinic/model/Vendedor.java
+++ src/main/java/org/springframework/samples/petclinic/model/Vendedor.java
@@ -4,6 +4,7 @@ package org.springframework.samples.petclinic.model;
 import java.util.Collection;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
@@ -11,6 +12,8 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Range;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -22,14 +25,19 @@ import lombok.Setter;
 
 public class Vendedor extends Persona {
 
+	@Column(name = "vacaciones")
 	private Boolean								vacaciones;
 
+	@Column(name = "valoracion")
+	@Range(min = 0, max = 5)
 	private Double								valoracion;
 
 	@NotEmpty
+	@Column(name = "nombre_tienda")
 	private String								nombreTienda;
 
 	@NotEmpty
+	@Column(name = "direccion_tienda")
 	private String								direccionTienda;
 
 	@OneToOne(cascade = CascadeType.ALL)
@@ -43,5 +51,12 @@ public class Vendedor extends Persona {
 	private Collection<@Valid Videojuego>		videojuegos;
 	@OneToMany
 	private Collection<@Valid Merchandasing>	merchandasings;
+
+
+	@Override
+	public String toString() {
+		return "Vendedor [ciudad=" + this.vacaciones + ", codigoPostal=" + this.nombreTienda + ", direccion=" + this.direccionTienda + ", peliculas=" + this.peliculas + ", merchandasings=" + this.merchandasings + ", user=" + this.user + ", videojuegos="
+			+ this.videojuegos + "]";
+	}
 
 }
