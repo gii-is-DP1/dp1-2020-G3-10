6d7e4fe96105a0195f51567429b7de45951102c4
Jaime Lucas Lozano <jailucloz@alum.us.es>
2021-02-07 21:54:35 +0100

    Revisión tests Merchandasing

--- src/test/java/org/springframework/samples/petclinic/service/MerchandasingServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/MerchandasingServiceTests.java
@@ -2,6 +2,8 @@ package org.springframework.samples.petclinic.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.assertj.core.api.Assertions;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.junit.jupiter.api.Test;
@@ -31,15 +33,18 @@ public class MerchandasingServiceTests {
 
 	}
 
-	// id = 31
+	
 	@Test
+	@Transactional
 	void saveMerchandasingTest() {
 		Merchandasing merchandasing = new Merchandasing();
 		merchandasing.setId(4);
 		merchandasing.setFabricante("BANDAI");
 		merchandasing.setNombre("Figura de Goku");
 		merchandasing.setPrecio(33.33);
+		merchandasing.setDescripcion("Esta es la descripción del producto de prueba 1, un goku super saiyan 4 del todo a cien");
 		merchandasing.setTipo(TipoMerchandasing.FIGURA);
+		merchandasing.setImagen("https://www.lamansiondelterror.es/4276-large_default/super-saiyan-4-son-goku-figura-15-cm-dragon-ball-super-son-goku-fes.jpg");
 		this.merchandasingService.saveMerchandasing(merchandasing);
 		List<Merchandasing> merchandasings = this.merchandasingService.findMerchandasings();
 		Merchandasing merchan = merchandasings.get(2);
@@ -48,23 +53,29 @@ public class MerchandasingServiceTests {
 		Assertions.assertThat(merchan.getId().equals(3));
 		Assertions.assertThat(merchan.getNombre().equals("Figura de Goku"));
 		Assertions.assertThat(merchan.getPrecio().equals(33.33));
+		Assertions.assertThat(merchan.getDescripcion().equals("Esta es la descripción del producto de prueba 1, un goku super saiyan 4 del todo a cien"));
 		Assertions.assertThat(merchan.getTipo().equals(TipoMerchandasing.FIGURA));
+		Assertions.assertThat(merchan.getImagen().equals("https://www.lamansiondelterror.es/4276-large_default/super-saiyan-4-son-goku-figura-15-cm-dragon-ball-super-son-goku-fes.jpg"));
 		Assertions.assertThat(merchan.getFabricante().equals("BANDAI"));
 
 	}
 
 	@Test
+	@Transactional
 	void deleteMerchandasingTest() {
-		this.merchandasingService.deleteMerchandasing(2);
+		this.merchandasingService.deleteMerchandasing(4);
 		List<Merchandasing> merchandasings = this.merchandasingService.findMerchandasings();
 		Assertions.assertThat(!merchandasings.isEmpty() && merchandasings.size() == 1);
 	}
 
 	@Test
+	@Transactional
 	void findMerchandasingByIdTest() {
 		Merchandasing merchandasing = this.merchandasingService.findMerchandasingById(2);
 		Assertions.assertThat(merchandasing.getNombre().equals("Goku SSJ4") && merchandasing.getPrecio().equals(33.33)
 				&& merchandasing.getFabricante().equals("Bandai") && merchandasing.getId().equals(2)
+				&& merchandasing.getDescripcion().equals("Un Goku plasticoso y chinoso más, tal cual ;)")
+				&& merchandasing.getImagen().equals("https://www.comicalia.com/39974-thickbox_default/super-saiyan-4-son-goku-figura-18-cm-dragon-ball-gt-full-scratch.jpg")
 				&& merchandasing.getTipo().equals(TipoMerchandasing.FIGURA));
 
 	}
